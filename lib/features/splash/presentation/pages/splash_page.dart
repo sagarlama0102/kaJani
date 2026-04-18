61a697c762418e88fa77248e3f0cfe986dd9ce30
sagarlama0102 <sagarlama0102@gmail.com>
2026-04-18 20:23:45 +0545

    designing splash page

--- lib/features/splash/presentation/pages/splash_page.dart
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -5,6 +5,23 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: 
+                  AssetImage('assets/kajanilogo.png'),
+                  fit: BoxFit.contain
+                ),
+                )
+              )
+            ],
+          ),
+        )),
+    );
   }
 }
